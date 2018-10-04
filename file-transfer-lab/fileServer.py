bbdd85060a0854c291bf238594d2f5a78da2ba8c
diana3982 <31858218+diana3982@users.noreply.github.com>
2018-10-03 23:04:59 -0600

    can successfully search for file

--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -6,27 +6,45 @@ host = ''
 sock.bind((host,2525))
 sock.listen(10) # listens for up to 10 incoming connections
 print('Server Active')
-ifFileFound = 0
 
 while True:
     print('... waiting to connect ...')
     connection, client_add = sock.accept()
-    recievedFile = connection.recv(1024)
-    currDir = os.getcwd()
-    for file in os.listdir(currDir):
-        if file == recievedFile:
-            recievedFile = 1
-            break
-    if recievedFile == 0:
-        print recievedFile + (" could not be found on the server")
+    cmd = connection.recv(1024)
+    print('connection esablished from ', repr(client_add))
+    print('Client $', (cmd))
+    if(cmd == 'quit'):
+        break
     else:
-        print(str(recievedFile)+ " was found!")
-        uploadFile = open(currDirrecievedFile,"rb")
-        readFile = uploadFile.read(1024)
-        while readFile:
-            connection.send(readFile)
-            readFile = uploadFile.read(1024)
-        print("File sent")
-    break
+        try:
+            string = cmd.split(' ', 1) # considers put and get
+            fileName = string[1]
+
+            if(string[0] == 'put'):
+                with open(fileName,'wb') as f:
+                    while True:
+                        print('... recieving file ...')
+                        data = sock.recv(1024)
+                        if not data:
+                            break
+                        f.write(data)
+                    f.close()
+                    print('File recieved')
+
+            elif(string[0] == 'get'):
+                try:
+                    with open(fileName, 'rb') as f:
+                        for data in f:
+                            connection.sendall(data)
+                    print('Successfully sent')
+                except IOError:
+                    print('File is not found on server!')
+                    break
+            else:
+                print('Unknown Command. Goodbye.')
+                break
+        except IndexError:
+            print('Unknown Command. Goodbye.')
+            break
 connection.close() # to make sure the connection is closed
 sock.close() # to make sure the socket is closed
